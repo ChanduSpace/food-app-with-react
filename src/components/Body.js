b032710d47f6363adf4382f7c8617051ec55cbf5
Chandu <dulamchandu13@gmail.com>
2024-02-29 01:43:16 +0530

    search box and filter function added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,13 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   let [restaurantList, setRestaurantList] = useState([]);
+  const [filteredRes, setFilteredRes] = useState([]);
+
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -12,17 +17,34 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await data.json();
-    console.log(jsonData);
-    console.log(
-      jsonData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
-    );
     setRestaurantList(
       jsonData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
     );
+    setFilteredRes(
+      jsonData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
   };
-  return (
+
+  return restaurantList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body-container">
       <div className="search-container">
+        <input
+          type="text"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            const filteredRestaurant = restaurantList.filter((res) =>
+              res.info.name.toLowerCase().includes(searchValue.toLowerCase())
+            );
+            setFilteredRes(filteredRestaurant);
+          }}
+        >
+          Search
+        </button>
         <button
           onClick={() => {
             const filteredList = restaurantList.filter(
@@ -35,7 +57,7 @@ const Body = () => {
         </button>
       </div>
       <div className="restaurant-container">
-        {restaurantList.map((restaurant) => (
+        {filteredRes.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             restaurantData={restaurant}
---
+++ src/components/Shimmer.js
@@ -0,0 +1,80 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+      <div className="shimmer-card">
+        <div className="img-shimmer"></div>
+        <div className="text-shimmer1"></div>
+        <div className="text-shimmer2"></div>
+        <div className="shine"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
