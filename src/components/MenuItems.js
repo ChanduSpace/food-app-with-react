6a4a1c5049e86cca0ef2e0bf5112b2368e3834dc
Chandu <dulamchandu13@gmail.com>
2024-03-02 21:48:17 +0530

    menu added

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -2,21 +2,22 @@ import { CDN_URL } from "../utils/constants";
 
 const MenuItems = (props) => {
   console.log(props);
-  const { name, price, imageId, description } = props.itemInfo.card.info;
+  const { name, price, imageId, description, defaultPrice } =
+    props.itemInfo.card.info;
 
   return (
     <div className="res-items-container">
       <div className="item-info">
-        <img
-          src="https://www.clipartmax.com/png/full/299-2998556_vegetarian-food-symbol-icon-non-veg-symbol-png.png"
-          alt="no-img"
-        />
+        <img src="https://www.clipartmax.com/png/full/299-2998556_vegetarian-food-symbol-icon-non-veg-symbol-png.png" />
         <h4>{name}</h4>
-        <h5>Rs.{price / 100}</h5>
+        <h5>Rs.{(price || defaultPrice) / 100}</h5>
         <p className="description">{description}</p>
       </div>
       <div className="item-img-container">
-        <img src={CDN_URL + imageId} />
+        <img
+          src={CDN_URL + imageId}
+          onError={(event) => (event.target.style.display = "none")}
+        />
       </div>
     </div>
   );
