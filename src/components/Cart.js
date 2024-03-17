cba8d4473aff5bfb3d357c6fc051052d4181c584
Chandu <dulamchandu13@gmail.com>
2024-03-17 12:35:14 +0530

    Fix build issues

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,23 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CDN_URL, NO_IMG } from "../utils/constants";
 import { NON_VEG_IMG } from "../utils/constants";
 import { VEG_IMG, NO_IMG } from "../utils/constants";
 import Items from "./Items";
 import store from "../utils/store";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
   console.log(cartItems);
+  const dispatch = useDispatch();
+  const handleClearAll = () => {
+    dispatch(clearCart());
+  };
 
   return (
     <div className="cart-item-container">
       <h1>Cart</h1>
+      <button onClick={() => handleClearAll()}>Clear Cart</button>
       {cartItems.map((items) => (
         <Items items={items} />
       ))}
